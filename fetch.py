0426ef63646d2c9ec01ad6cd2b1f69ad07e74bed
Richard Paul <richard@primarysite.net>
2017-03-01 16:31:50 +0000

    * Added requirements.txt * Changing references to url to website near everywhere * Changing the list of websites from a string into a dictionary structure * Changed the request to build the string from the new dict structure * Added an async timeout to the fetcher function * Renamed the run function

--- fetch.py
+++ fetch.py
@@ -3,51 +3,84 @@
 import time
 import asyncio
 import uvloop
-from aiohttp import ClientSession
+import aiohttp
+import async_timeout
 
 
 websites = [
-    'http://www.google.co.uk',
-    'http://www.bing.co.uk',
-    'https://uk.yahoo.com',
-    'http://www.bbc.co.uk',
-    'https://www.sky.com',
-    'https://www.richardnpaul.co.uk/foo',
+    {
+        'domain': 'google.co.uk',
+        'scheme': 'http',
+        'prefix': 'www',
+        'suffix': ''
+    },
+    {
+        'domain': 'bing.co.uk',
+        'scheme': 'http',
+        'prefix': 'www',
+        'suffix': ''
+    },
+    {
+        'domain': 'yahoo.com',
+        'scheme': 'https',
+        'prefix': 'uk',
+        'suffix': ''
+    },
+    {
+        'domain': 'bbc.co.uk',
+        'scheme': 'http',
+        'prefix': 'www',
+        'suffix': ''
+    },
+    {
+        'domain': 'sky.com',
+        'scheme': 'https',
+        'prefix': 'www',
+        'suffix': ''
+    },
+    {
+        'domain': 'richardnpaul.co.uk',
+        'scheme': 'https',
+        'prefix': 'www',
+        'suffix': 'foo'
+    }
 ]
 
 
-async def fetch(url, session):
-    start = time.time()
-    async with session.get(url) as response:
-        trip = time.time() - start
-        print("{0},{1},{2}".format(response.url, response.status, trip))
-        #return await response.read()
+async def fetch(w, session):
+    with async_timeout.timeout(10):
+        start = time.time()
+        async with session.get('{0}://{1}.{2}/{3}'.format(w['scheme'],w['prefix'],w['domain'],w['suffix'])) as response:
+            trip = time.time() - start
+            print("{0},{1},{2}".format(response.url, response.status, trip))
+            #return await response.read()
 
 
-async def bound_fetch(sem, url, session):
+async def bound_fetch(sem, website, session):
     # Getter function with semaphore.
     async with sem:
-        await fetch(url, session)
+        await fetch(website, session)
 
 
-async def run(urls):
+async def fetch_websites(websites):
     tasks = []
     # create instance of Semaphore and pass a request number limit
     sem = asyncio.Semaphore(1000)
 
     # Create client session that will ensure we don't open a new connection
     # per each request.
-    async with ClientSession() as session:
-        for url in urls:
+    async with aiohttp.ClientSession() as session:
+        for website in websites:
             # pass Semaphore and session to every GET request
-            task = asyncio.ensure_future(bound_fetch(sem, url, session))
+            task = asyncio.ensure_future(bound_fetch(sem, website, session))
             tasks.append(task)
 
         responses = asyncio.gather(*tasks)
         await responses
 
+
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 loop = asyncio.get_event_loop()
-future = asyncio.ensure_future(run(websites))
+future = asyncio.ensure_future(fetch_websites(websites))
 loop.run_until_complete(future)
 
