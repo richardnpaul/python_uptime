d2cfdbb3ebfa831cdf25f193837425a7966397ba
Richard Paul <richard@primarysite.net>
2017-03-09 19:22:16 +0000

    * Adding and id key to the website dict objects

--- websites.py
+++ websites.py
@@ -1,35 +1,41 @@
 website_list = [
     {
+        'id': 1,
         'domain': 'google.co.uk',
         'scheme': 'http',
         'prefix': 'www',
         'suffix': ''
     },
     {
+        'id': 2,
         'domain': 'bing.co.uk',
         'scheme': 'http',
         'prefix': 'www',
         'suffix': ''
     },
     {
+        'id': 3,
         'domain': 'yahoo.com',
         'scheme': 'https',
         'prefix': 'uk',
         'suffix': ''
     },
     {
+        'id': 4,
         'domain': 'bbc.co.uk',
         'scheme': 'http',
         'prefix': 'www',
         'suffix': ''
     },
     {
+        'id': 5,
         'domain': 'sky.com',
         'scheme': 'https',
         'prefix': 'www',
         'suffix': ''
     },
     {
+        'id': 6,
         'domain': 'richardnpaul.co.uk',
         'scheme': 'https',
         'prefix': 'www',
